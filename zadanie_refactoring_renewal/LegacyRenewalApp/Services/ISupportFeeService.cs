4c6ed990b381983cd57d06e197c1485cccdc9fed
Eryk <s31954@pjwstk.edu.pl>
2026-04-10 23:23:20 +0200

    add SupportFeeService

---
+++ zadanie_refactoring_renewal/LegacyRenewalApp/Services/ISupportFeeService.cs
@@ -0,0 +1,6 @@
+﻿using LegacyRenewalApp.Models;
+namespace LegacyRenewalApp.Services;
+
+public interface ISupportFeeService {
+    public FeeResult Calculate(bool includePremiumSupport, string planCode);
+}
---
+++ zadanie_refactoring_renewal/LegacyRenewalApp/Services/SupportFeeService.cs
@@ -0,0 +1,33 @@
+﻿using LegacyRenewalApp.Models;
+namespace LegacyRenewalApp.Services;
+
+public class SupportFeeService : ISupportFeeService {
+
+    public FeeResult Calculate(bool includePremiumSupport, string planCode) {
+        
+        decimal supportFee = 0m;
+        string notes = string.Empty;
+        
+        
+        if (includePremiumSupport)
+        {
+            if (planCode == "START")
+            {
+                supportFee = 250m;
+            }
+            else if (planCode == "PRO")
+            {
+                supportFee = 400m;
+            }
+            else if (planCode == "ENTERPRISE")
+            {
+                supportFee = 700m;
+            }
+
+            notes += "premium support included; ";
+        }
+        
+        return new FeeResult(supportFee, notes);
+    }
+
+}
